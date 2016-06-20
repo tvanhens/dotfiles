317ccf17440557512ee3aa6961a03f4e3586c078
Tyler van Hensbergen <tvanhens@Tylers-MacBook-Pro.local>
2016-06-19 23:23:12 -0400

    Go mode and clj-refactor

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -26,7 +26,7 @@
 (global-set-key (kbd "C-c n") 'indent-region)
 
 ;; Theme:
-(load-theme 'zenburn t)
+(load-theme 'monokai t)
 
 ;; Paredit
 (autoload 'enable-paredit-mode "paredit" "Turn on pseudo-structural editing of Lisp code." t)
@@ -61,8 +61,41 @@
 ;; Cider
 (add-hook 'cider-mode-hook #'eldoc-mode)
 
+;; Clj-Refactor
+(require 'clj-refactor)
+
+(defun my-clojure-mode-hook ()
+    (clj-refactor-mode 1)
+    (yas-minor-mode 1) ; for adding require/use/import statements
+    ;; This choice of keybinding leaves cider-macroexpand-1 unbound
+    (cljr-add-keybindings-with-prefix "C-c C-m"))
+
+(add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
+;; Neo Tree
 (require 'neotree)
 (global-set-key [f8] 'neotree-toggle)
+
+;;------------------------------------------------------------------------------
+;; Go Mode
+
+(require 'go-mode-autoloads)
+(add-hook 'go-mode-hook
+          (lambda ()
+            (add-hook 'before-save-hook 'gofmt-before-save)
+            (setq tab-width 4)
+            (setq indent-tabs-mode 1)))
+
+(require 'go-autocomplete)
+(require 'auto-complete-config)
+(ac-config-default)
+
+(setq company-tooltip-limit 20)
+(setq company-idle-delay .3)
+(setq company-echo-delay 0)
+(setq company-begin-commands '(self-insert-command))
+
+(require 'company-go)
 
 ;;------------------------------------------------------------------------------
 ;; Markdown
