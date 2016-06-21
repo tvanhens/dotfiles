6557597f49cd1eb148487adbfcab06621003fffd
Tyler van Hensbergen <tvanhens@Tylers-MacBook-Pro.local>
2016-06-21 11:10:52 -0400

    Inhibit startup messages and add smart-mode-line

--- emacs.d/global.el
+++ emacs.d/global.el
@@ -1,4 +1,15 @@
 ;;------------------------------------------------------------------------------
 ;; Global
 
+;; Show line numbers everywhere
+(global-linum-mode)
+
+;; Smart mode line
+(setq sml/theme 'dark)
+(sml/setup)
+
+;; Turn off welcome message
+(setq inhibit-startup-message t)
+
+;; Nyan cat
 (add-hook 'emacs-startup-hook 'nyan-mode)
--- emacs.d/init.el
+++ emacs.d/init.el
@@ -122,6 +122,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default)))
  '(ibuffer-saved-filter-groups nil)
  '(ibuffer-saved-filters
    (quote
